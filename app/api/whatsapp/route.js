8d70b214e43a988c227feb2dadd15e2f3a8681fc
Naman Chawhan <chawhannaman5@gmail.com>
2026-03-15 15:31:55 +0530

    Added supabase

--- app/api/whatsapp/route.js
+++ app/api/whatsapp/route.js
@@ -1,21 +1,37 @@
+import { supabase } from "@/lib/supabase";
 import twilio from "twilio";
 
 export const runtime = "nodejs";
-export const dynamic = "force-dynamic";
 
 export async function POST(req) {
 
   const formData = await req.formData();
   const message = formData.get("Body");
 
-  console.log("Incoming message:", message);
-
   const twiml = new twilio.twiml.MessagingResponse();
 
-  if (message?.toLowerCase() === "get") {
-    twiml.message("Analytics feature coming soon 📊");
+  if (message.toLowerCase() === "get") {
+
+    const { data } = await supabase
+      .from("expenses")
+      .select("*");
+
+    const total = data.reduce((s,e)=>s+e.amount,0);
+
+    twiml.message(`Total spent ₹${total}`);
+
   } else {
-    twiml.message("Expense received ✅");
+
+    const [amount,item,category,payment] = message.split(" ");
+
+    await supabase.from("expenses").insert([{
+      amount,
+      item,
+      category,
+      payment
+    }]);
+
+    twiml.message("Expense saved ✅");
   }
 
   return new Response(twiml.toString(), {
